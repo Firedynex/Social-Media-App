9e52ba4547ac28f660528d7afab658c70376c257
Firedynex <58797454+Firedynex@users.noreply.github.com>
2024-07-29 17:51:11 -0400

    Added routing

--- Frontend/SocialMediaFrontend/src/main.jsx
+++ Frontend/SocialMediaFrontend/src/main.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Login from './Login/Login.jsx'
 import RegistrationPage from './RegistrationPages/RegistrationPage.jsx'
 
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login/>
+  },
+  {
+    path: "/register",
+    element: <RegistrationPage/>
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Login />
-    <RegistrationPage></RegistrationPage>
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
