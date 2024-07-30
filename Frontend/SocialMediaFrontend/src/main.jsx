1a3f5a9465b896ebbdc520c39dd092627ee03044
Matthew Dinh <matthewddinh@gmail.com>
2024-07-30 18:03:23 -0400

    commit 1

--- Frontend/SocialMediaFrontend/src/main.jsx
+++ Frontend/SocialMediaFrontend/src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Login from './Login/Login.jsx'
 import RegistrationPage from './RegistrationPages/RegistrationPage.jsx'
+import EventCreator from './EventCreator/EventCreator.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Login />
     <RegistrationPage></RegistrationPage>
+
   </React.StrictMode>,
 )
