c2ad8d7973f74a152385474cdcf0021750f23e93
Firedynex <58797454+Firedynex@users.noreply.github.com>
2024-08-04 19:39:45 -0400

    Added not null constraints to db

--- SQL Files/Database Script.sql
+++ SQL Files/Database Script.sql
@@ -1,28 +1,28 @@
 CREATE DATABASE SocialMediaDB;
 USE SocialMediaDB;
 CREATE TABLE users(
-	email VARCHAR(255),
-    username VARCHAR(20),
+	email VARCHAR(255) NOT NULL,
+    username VARCHAR(20) NOT NULL,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
-    pass VARCHAR(50)
+    pass VARCHAR(50) NOT NULL
 );
 CREATE TABLE normal_lists(
-    list_name VARCHAR(50),
-    list_description VARCHAR(255),
-    list_content VARCHAR(255),
-    author VARCHAR(20),
-    list_url VARCHAR(100)
+    list_name VARCHAR(50) NOT NULL,
+    list_description VARCHAR(255) NOT NULL,
+    list_content VARCHAR(255) NOT NULL,
+    author VARCHAR(20) NOT NULL,
+    list_url VARCHAR(100) NOT NULL
 );
 CREATE TABLE events(
     organizer VARCHAR(100),
     event_name VARCHAR(50),
     event_location VARCHAR(70),
-    event_date DATE,
-    event_time TIME,
-    event_description VARCHAR(255),
+    event_date DATE NOT NULL,
+    event_time TIME NOT NULL,
+    event_description VARCHAR(255) NOT NULL,
     email_reminder BOOLEAN,
     cars_needed BOOLEAN,
-    author VARCHAR(20),
+    author VARCHAR(20) NOT NULL,
 )
 SELECT * FROM users;
