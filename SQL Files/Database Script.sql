69d8faa1e899b2f32d55d67b08939607b37f871b
Firedynex <58797454+Firedynex@users.noreply.github.com>
2024-08-04 18:10:28 -0400

    Added backend starting code

---
+++ SQL Files/Database Script.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE SocialMediaDB;
+USE SocialMediaDB;
+CREATE TABLE users(
+	email VARCHAR(255),
+    username VARCHAR(20),
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    pass VARCHAR(50)
+);
+CREATE TABLE 
+SELECT * FROM users;
