603192ffa3c090dd087ef37eb8aed8a562d845bd
Firedynex <58797454+Firedynex@users.noreply.github.com>
2024-08-04 18:29:30 -0400

    Added events and normal lists table

--- SQL Files/Database Script.sql
+++ SQL Files/Database Script.sql
@@ -7,5 +7,22 @@ CREATE TABLE users(
     last_name VARCHAR(50),
     pass VARCHAR(50)
 );
-CREATE TABLE 
+CREATE TABLE normal_lists(
+    list_name VARCHAR(50),
+    list_description VARCHAR(255),
+    list_content VARCHAR(255),
+    author VARCHAR(20),
+    list_url VARCHAR(100)
+);
+CREATE TABLE events(
+    organizer VARCHAR(100),
+    event_name VARCHAR(50),
+    event_location VARCHAR(70),
+    event_date DATE,
+    event_time TIME,
+    event_description VARCHAR(255),
+    email_reminder BOOLEAN,
+    cars_needed BOOLEAN,
+    author VARCHAR(20),
+)
 SELECT * FROM users;
