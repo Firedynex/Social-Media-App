5030c481d9e933ccf328b78bfedc00836057c9bf
HhRyo <88109093+HhRyo@users.noreply.github.com>
2024-11-14 18:09:02 -0500

    Updated Login.jsx
    Is able to go to registration pages on sign up and movie list page on login (still need to work on forgot password)

--- Frontend/SocialMediaFrontend/src/Login/Login.jsx
+++ Frontend/SocialMediaFrontend/src/Login/Login.jsx
@@ -1,21 +1,42 @@
 import TitleBar from "../UniversalComponents/TitleBar/TitleBar.jsx";
-
-import "./Login.css";
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
-    return(
+    const navigate = useNavigate();
+
+    const handleLoginClick = () => {
+        navigate('../movieListPage'); //home page? 
+    };
+
+    const handleSignUpClick = () => {
+        navigate('/register'); 
+    };
+
+    return (
         <>
             <TitleBar />
             <div className="login-card">
                 <div className="page-header">
                     <p>Login</p>
                 </div>
-                <input className="login-input" id="userName" type="text" placeholder="Username" required/>
-                <input className= "login-input" id="password" type="password" placeholder="Password" required/>
+                <input className="login-input" id="userName" type="text" placeholder="Username" required />
+                <input className="login-input" id="password" type="password" placeholder="Password" required />
                 <a href="www.google.com">Forgot Password?</a>
                 <div className="button-div">
-                    <button className="login-buttons">Login</button>
-                    <button className="login-buttons">Sign Up</button>
+                    <button
+                        type="button"
+                        className="login-buttons"
+                        onClick={handleLoginClick}
+                    >
+                        Login
+                    </button>
+                    <button
+                        type="button"
+                        className="login-buttons"
+                        onClick={handleSignUpClick}
+                    >
+                        Sign Up
+                    </button>
                 </div>
             </div>
         </>
